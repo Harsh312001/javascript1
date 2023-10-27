50a9c9da442d403e67af5979910e64c7214f7fd7
Harsh312001 <102729661+Harsh312001@users.noreply.github.com>
2023-10-27 04:46:52 +0000

    change

--- 01_basics/test1.js
+++ 01_basics/test1.js
@@ -9,5 +9,18 @@ console.log("harsh")
 //number
 //symbol
 
-console.log(typeof(undefined))
-console.log(typeof(null))
+// console.log(typeof(undefined))
+// console.log(typeof(null))
+// let num="33";
+// console.log(typeof num)
+// let shiv=Number(num)
+// console.log(typeof shiv)
+//notes
+
+//when we are converting a numer we can easily convert in easy way
+//when we convert a a;phabet into number we cant onvet and ans is Nan not a number 
+//true =1 and else false =0
+
+let islogeed="";
+let data=Boolean(islogeed)
+console.log(data)
