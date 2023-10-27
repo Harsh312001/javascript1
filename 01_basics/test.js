345ae56ed9edf1917ab211bafa37200a6cdfb1e9
Harsh312001 <102729661+Harsh312001@users.noreply.github.com>
2023-10-27 03:33:41 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
 console.log("harsh")
+var e="harsh maheshwari"
+const number=12344
+let num="shicam kumar"
+console.table([e,number,num])
---
+++ 01_basics/test1.js
@@ -0,0 +1,13 @@
+console.log("harsh")
+//datatypes
+//bigint
+//int
+//string=""
+//null
+//undefined
+//boolean
+//number
+//symbol
+
+console.log(typeof(undefined))
+console.log(typeof(null))
